c5cd916d8ecca64ec9273f1c737b91f34b3ac8c9
Vincent <vincentredrum@gmail.com>
2014-07-30 16:05:27 +0100

    Extracted WithLayout to a new class and rename previous class to MVCViewResultBase -> SRP

--- AssertMVC/MVCViewResult.cs
+++ AssertMVC/MVCViewResult.cs
@@ -3,7 +3,7 @@ using System.Web.Mvc;
 
 namespace AssertMVC
 {
-    public static class MVCViewResult
+    public static class MVCViewResultBase
     {
         public static void WithName(this ViewResultBase result, string expectedName)
         {
@@ -15,15 +15,18 @@ namespace AssertMVC
             Assert.AreEqual(string.Empty, result.ViewName);
         }
 
-        public static void WithLayout(this ViewResult result, string layoutName)
-        {
-            Assert.AreEqual(layoutName, result.MasterName);
-        }
-
         public static MVCModel WithModel(this ViewResultBase result)
         {
             Assert.IsNotNull(result.Model);
             return new MVCModel(result.Model);
+        }
+    }
+
+    public static class MVCViewResult
+    {
+        public static void WithLayout(this ViewResult result, string layoutName)
+        {
+            Assert.AreEqual(layoutName, result.MasterName);
         }
     }
 }
