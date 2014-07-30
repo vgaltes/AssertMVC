741af2660e9e8776867adebc26200370b2efead0
Vincent <vincentredrum@gmail.com>
2014-07-30 14:01:42 +0100

    Initial project structure. TestConvertToViewResult -> red

---
+++ AssertMVC.Tests/MVCActionResultTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace AssertMVC.Tests
+{
+    [TestClass]
+    public class MVCActionResultTest
+    {
+        [TestMethod]
+        public void TestConvertToViewResult()
+        {
+            ActionResult viewResult = new ViewResult();
+
+            Assert.IsNotNull(viewResult.AsViewResult());
+
+            viewResult = new PartialViewResult();
+            Assert.IsNull(viewResult.AsViewResult());
+        }
+    }
+}
