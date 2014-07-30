8237e74524b2d6ea8be7fc901c1f9c230b67b690
Vincent <vincentredrum@gmail.com>
2014-07-30 14:13:00 +0100

    TestViewHasName -> red

---
+++ AssertMVC.Tests/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AssertMVC.Tests.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult IndexView(string nameView)
+        {
+            return View(nameView);
+        }
+    }
+}
---
+++ AssertMVC.Tests/MVCViewResultTest.cs
@@ -0,0 +1,25 @@
+﻿using AssertMVC.Tests.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssertMVC.Tests
+{
+    [TestClass]
+    public class MVCViewResultTest
+    {
+        [TestMethod]
+        public void TestViewHasName()
+        {
+            string viewName = "TestViewName";
+
+            var controller = new TestController();
+            var result = controller.IndexView(viewName);
+
+            result.AsViewResult().HasName(viewName);
+        }
+    }
+}
