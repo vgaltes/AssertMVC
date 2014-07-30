f964602a9cae6a3110550aecf7bda2b6daba1f75
Vincent <vincentredrum@gmail.com>
2014-07-30 14:49:14 +0100

    Refactor. Put MVCModel in his own file.

--- AssertMVC/MVCViewResult.cs
+++ AssertMVC/MVCViewResult.cs
@@ -26,19 +26,4 @@ namespace AssertMVC
             return new MVCModel(result.Model);
         }
     }
-
-    public class MVCModel
-    {
-        object model = null;
-
-        public MVCModel(object model)
-        {
-            this.model = model;
-        }
-
-        public void OfType<T>()
-        {
-            Assert.IsInstanceOfType(model, typeof(T));
-        }
-    }
 }
