23b950995772d53de2542364f62d7dfe35df6aba
Vincent <vincentredrum@gmail.com>
2014-07-30 14:15:35 +0100

    TestViewHasName -> green

--- AssertMVC.Tests/MVCActionResultTest.cs
+++ AssertMVC.Tests/MVCActionResultTest.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Web.Mvc;
-using AssertMVC;
 
 namespace AssertMVC.Tests
 {
---
+++ AssertMVC/MVCViewResult.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace AssertMVC
+{
+    public static class MVCViewResult
+    {
+        public static void HasName(this ViewResult result, string expectedName)
+        {
+            Assert.AreEqual(expectedName, result.ViewName);
+        }
+    }
+}
