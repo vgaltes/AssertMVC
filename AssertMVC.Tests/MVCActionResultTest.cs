6cd34d35b27d6b0a223897577bbea5191fd5426b
Vincent <vincentredrum@gmail.com>
2014-07-30 14:08:15 +0100

    TestConvertToViewResult -> green

--- AssertMVC.Tests/MVCActionResultTest.cs
+++ AssertMVC.Tests/MVCActionResultTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Web.Mvc;
+using AssertMVC;
 
 namespace AssertMVC.Tests
 {
---
+++ AssertMVC/MVCActionResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AssertMVC
+{
+    public static class MVCActionResult
+    {
+        public static ViewResult AsViewResult(this ActionResult result)
+        {
+            return result as ViewResult;
+        }
+    }
+}
