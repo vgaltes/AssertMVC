5287839d0b26c333b83d1ab70dc3cda6f0e0d906
Vincent <vincentredrum@gmail.com>
2014-07-30 16:06:30 +0100

    Extract MVCViewResultBase to it's own file

--- AssertMVC/MVCViewResult.cs
+++ AssertMVC/MVCViewResult.cs
@@ -3,25 +3,6 @@ using System.Web.Mvc;
 
 namespace AssertMVC
 {
-    public static class MVCViewResultBase
-    {
-        public static void WithName(this ViewResultBase result, string expectedName)
-        {
-            Assert.AreEqual(expectedName, result.ViewName);
-        }
-
-        public static void Default(this ViewResultBase result)
-        {
-            Assert.AreEqual(string.Empty, result.ViewName);
-        }
-
-        public static MVCModel WithModel(this ViewResultBase result)
-        {
-            Assert.IsNotNull(result.Model);
-            return new MVCModel(result.Model);
-        }
-    }
-
     public static class MVCViewResult
     {
         public static void WithLayout(this ViewResult result, string layoutName)
---
+++ AssertMVC/MVCViewResultBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace AssertMVC
+{
+    public static class MVCViewResultBase
+    {
+        public static void WithName(this ViewResultBase result, string expectedName)
+        {
+            Assert.AreEqual(expectedName, result.ViewName);
+        }
+
+        public static void Default(this ViewResultBase result)
+        {
+            Assert.AreEqual(string.Empty, result.ViewName);
+        }
+
+        public static MVCModel WithModel(this ViewResultBase result)
+        {
+            Assert.IsNotNull(result.Model);
+            return new MVCModel(result.Model);
+        }
+    }
+}
